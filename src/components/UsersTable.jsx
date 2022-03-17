5d0de134579aac4b0c451033becbccae1ce77910
nick-here <zuraniewski.karol@gmail.com>
2022-03-17 08:46:41 +0100

    users table columns

---
+++ src/components/UsersTable.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+	{ field: 'lp', headerName: 'LP', width: 50 },
+	{ field: 'data', headerName: 'Data', width: 150 },
+	{ field: 'nrUmowy', headerName: 'Numer umowy', width: 150 },
+	{ field: 'firma', headerName: 'Firma', width: 150 },
+	{ field: 'nip', headerName: 'NIP', width: 150 },
+	{ field: 'adres', headerName: 'Adres', width: 150 },
+	{ field: 'reprezentant', headerName: 'Reprezentant', width: 150 },
+	{ field: 'telefon', headerName: 'Telefon', width: 150 },
+	{ field: 'email', headerName: 'E-mail', width: 150 },
+	{ field: 'calkowita', headerName: 'Kwota całkowita', width: 150 },
+	{ field: 'calkowitaSlownie', headerName: 'Słownie', width: 150 },
+	{ field: 'rata1', headerName: 'Rata 1', width: 150 },
+	{ field: 'rata1Slownie', headerName: 'Słownie', width: 150 },
+	{ field: 'rata2', headerName: 'Rata 2', width: 150 },
+	{ field: 'rata2Slownie', headerName: 'Słownie', width: 150 },
+	{ field: 'lh', headerName: 'LH', width: 150 },
+];
+
+const UsersTable = () => {
+	return;
+};
+
+export default UsersTable;
