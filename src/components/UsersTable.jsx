a5ed9f65b73c23cb7f340a7ab1e27f52e56f52d5
nick-here <zuraniewski.karol@gmail.com>
2022-03-17 10:48:00 +0100

    DataGrid displaying users

--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
 import getRandomUsers from '../testing/getRandomUsers.js';
 import { Edit, Delete } from '@mui/icons-material';
+
+// TODO: validation
+// TODO: row deletion
 
 const columns = [
 	{
@@ -13,27 +16,48 @@ const columns = [
 			<GridActionsCellItem icon={<Edit />} label='Edytuj' />,
 		],
 	},
-	{ field: 'id', headerName: 'LP', width: 100, type: 'number' },
-	{ field: 'data', headerName: 'Data', width: 150 },
-	{ field: 'nrUmowy', headerName: 'Numer umowy', width: 150 },
-	{ field: 'firma', headerName: 'Firma', width: 150 },
-	{ field: 'nip', headerName: 'NIP', width: 150 },
-	{ field: 'adres', headerName: 'Adres', width: 150 },
-	{ field: 'reprezentant', headerName: 'Reprezentant', width: 150 },
-	{ field: 'telefon', headerName: 'Telefon', width: 150 },
-	{ field: 'email', headerName: 'E-mail', width: 150 },
-	{ field: 'calkowita', headerName: 'Kwota całkowita', width: 150, type: 'number' },
+	{ field: 'lp', headerName: 'LP', width: 100, type: 'number' },
+	{ field: 'data', headerName: 'Data', width: 150, editable: true },
+	{ field: 'nrUmowy', headerName: 'Numer umowy', width: 150, editable: true },
+	{ field: 'firma', headerName: 'Firma', width: 150, editable: true },
+	{ field: 'nip', headerName: 'NIP', width: 150, editable: true },
+	{ field: 'adres', headerName: 'Adres', width: 150, editable: true },
+	{ field: 'reprezentant', headerName: 'Reprezentant', width: 150, editable: true },
+	{ field: 'telefon', headerName: 'Telefon', width: 150, editable: true },
+	{ field: 'email', headerName: 'E-mail', width: 150, editable: true },
+	{
+		field: 'calkowita',
+		headerName: 'Kwota całkowita',
+		width: 150,
+		type: 'number',
+		editable: true,
+	},
 	{ field: 'calkowitaSlownie', headerName: 'Słownie', width: 150 },
-	{ field: 'rata1', headerName: 'Rata 1', width: 150, type: 'number' },
+	{ field: 'rata1', headerName: 'Rata 1', width: 150, type: 'number', editable: true },
 	{ field: 'rata1Slownie', headerName: 'Słownie', width: 150 },
-	{ field: 'rata2', headerName: 'Rata 2', width: 150, type: 'number' },
+	{ field: 'rata2', headerName: 'Rata 2', width: 150, type: 'number', editable: true },
 	{ field: 'rata2Slownie', headerName: 'Słownie', width: 150 },
 ];
 
 const rows = getRandomUsers(25);
 
 const UsersTable = () => {
-	return <DataGrid rows={rows} columns={columns} />;
+	const handleCellEditCommit = React.useCallback(async params => {
+		console.log('cell saved: ', params);
+		// TODO: server communication
+	}, []);
+
+	return (
+		<DataGrid
+			editMode='row'
+			onCellEditCommit={handleCellEditCommit}
+			rows={rows}
+			columns={columns}
+			components={{ Toolbar: GridToolbar }}
+			getRowId={row => row.lp}
+			// rowsPerPageOptions={[5, 15, 50]}
+		/>
+	);
 };
 
 export default UsersTable;
