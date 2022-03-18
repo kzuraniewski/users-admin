27ef706a759e475ffbab9bc486ed50be112bbfd2
nick-here <zuraniewski.karol@gmail.com>
2022-03-18 23:04:56 +0100

    single list item

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -6,8 +6,7 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/Inbox';
-import MailIcon from '@mui/icons-material/Mail';
+import GroupIcon from '@mui/icons-material/Group';
 import Divider from '@mui/material/Divider';
 
 const drawerWidth = 240;
@@ -19,31 +18,19 @@ const Sidenav = () => {
 			sx={{
 				width: drawerWidth,
 				flexShrink: 0,
-				[`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+				[`& .MuiDrawer-paper`]: { width: drawerWidth },
 			}}
+			// PaperProps={{ sx: { backgroundColor: 'primary.main' } }}
 		>
 			<Toolbar />
 			<Box sx={{ overflow: 'auto' }}>
 				<List>
-					{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
-				</List>
-				<Divider />
-				<List>
-					{['All mail', 'Trash', 'Spam'].map((text, index) => (
-						<ListItem button key={text}>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItem>
-					))}
+					<ListItem button>
+						<ListItemIcon>
+							<GroupIcon />
+						</ListItemIcon>
+						<ListItemText primary='Klienci' />
+					</ListItem>
 				</List>
 			</Box>
 		</Drawer>
