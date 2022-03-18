bc0073746b935353dff61da6c01946a4a9c2c8bb
nick-here <zuraniewski.karol@gmail.com>
2022-03-18 22:43:17 +0100

    template sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import Toolbar from '@mui/material/Toolbar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/Inbox';
+import MailIcon from '@mui/icons-material/Mail';
+import Divider from '@mui/material/Divider';
+
+const drawerWidth = 240;
 
 const Sidenav = () => {
 	return (
-		<div className='sidenav'>
-			<a href='#' className='sidenav__link'>
-				Użytkownicy
-			</a>
-			<a href='#' className='sidenav__link'>
-				Dodaj
-			</a>
-
-			<div className='sidenav__divider' />
-
-			<a href='#' className='sidenav__link'>
-				Konto
-			</a>
-		</div>
+		<Drawer
+			variant='permanent'
+			sx={{
+				width: drawerWidth,
+				flexShrink: 0,
+				[`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+			}}
+		>
+			<Toolbar />
+			<Box sx={{ overflow: 'auto' }}>
+				<List>
+					{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>
+								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+				<Divider />
+				<List>
+					{['All mail', 'Trash', 'Spam'].map((text, index) => (
+						<ListItem button key={text}>
+							<ListItemIcon>
+								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItem>
+					))}
+				</List>
+			</Box>
+		</Drawer>
 	);
 };
 
