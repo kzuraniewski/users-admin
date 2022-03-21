394117e93405fc27a502c10555405671189eea48
nick-here <zuraniewski.karol@gmail.com>
2022-03-21 11:53:48 +0100

    customers table external data

--- src/components/customers/CustomersPanel.jsx
+++ src/components/customers/CustomersPanel.jsx
@@ -1,12 +1,38 @@
 import { Box } from '@mui/material';
 import React, { useState } from 'react';
+import getRandomUsers from '../../testing/getRandomUsers.js';
 import CustomersTable from './CustomersTable.jsx';
+
+const columns = [
+	{ field: 'lp', headerName: 'LP', type: 'number' },
+	{ field: 'data', headerName: 'Data' },
+	{ field: 'nrUmowy', headerName: 'Nr umowy', width: 120 },
+	{ field: 'firma', headerName: 'Firma', width: 350 },
+	{ field: 'nip', headerName: 'NIP', width: 120 },
+	{ field: 'adres', headerName: 'Adres', width: 300 },
+	{ field: 'reprezentant', headerName: 'Reprezentant', width: 170 },
+	{ field: 'telefon', headerName: 'Telefon', width: 150 },
+	{ field: 'email', headerName: 'E-mail', width: 200 },
+	{
+		field: 'calkowita',
+		headerName: 'Kwota całkowita',
+		width: 130,
+		type: 'number',
+	},
+	{ field: 'calkowitaSlownie', headerName: 'Słownie', width: 220 },
+	{ field: 'rata1', headerName: 'Rata 1', width: 130, type: 'number' },
+	{ field: 'rata1Slownie', headerName: 'Słownie', width: 220 },
+	{ field: 'rata2', headerName: 'Rata 2', width: 130, type: 'number' },
+	{ field: 'rata2Slownie', headerName: 'Słownie', width: 220 },
+];
+
+const rows = getRandomUsers(25);
 
 const Panel = props => {
 	return (
 		<>
 			<Box height='80vh' padding='45px 30px' {...props}>
-				<CustomersTable />
+				<CustomersTable columns={columns} rows={rows} getRowId={row => row.lp} />
 			</Box>
 		</>
 	);
--- src/components/customers/CustomersTable.jsx
+++ src/components/customers/CustomersTable.jsx
@@ -6,36 +6,10 @@ import {
 	GridToolbarDensitySelector,
 	GridToolbarExport,
 	GridToolbarFilterButton,
+	DataGridProps,
 } from '@mui/x-data-grid';
-import getRandomUsers from '../../testing/getRandomUsers.js';
 import { Button } from '@mui/material';
 import Add from '@mui/icons-material/Add';
-
-const columns = [
-	{ field: 'lp', headerName: 'LP', type: 'number' },
-	{ field: 'data', headerName: 'Data' },
-	{ field: 'nrUmowy', headerName: 'Nr umowy', width: 120 },
-	{ field: 'firma', headerName: 'Firma', width: 350 },
-	{ field: 'nip', headerName: 'NIP', width: 120 },
-	{ field: 'adres', headerName: 'Adres', width: 300 },
-	{ field: 'reprezentant', headerName: 'Reprezentant', width: 170 },
-	{ field: 'telefon', headerName: 'Telefon', width: 150 },
-	{ field: 'email', headerName: 'E-mail', width: 200 },
-	{
-		field: 'calkowita',
-		headerName: 'Kwota całkowita',
-		width: 130,
-		type: 'number',
-		editable: true,
-	},
-	{ field: 'calkowitaSlownie', headerName: 'Słownie', width: 220 },
-	{ field: 'rata1', headerName: 'Rata 1', width: 130, type: 'number' },
-	{ field: 'rata1Slownie', headerName: 'Słownie', width: 220 },
-	{ field: 'rata2', headerName: 'Rata 2', width: 130, type: 'number' },
-	{ field: 'rata2Slownie', headerName: 'Słownie', width: 220 },
-];
-
-const rows = getRandomUsers(25);
 
 const Toolbar = () => {
 	return (
@@ -52,16 +26,11 @@ const Toolbar = () => {
 	);
 };
 
+/**
+ * @param {DataGridProps} props
+ */
 const UsersTable = props => {
-	return (
-		<DataGrid
-			rows={rows}
-			columns={columns}
-			components={{ Toolbar }}
-			getRowId={row => row.lp}
-			// rowsPerPageOptions={[5, 15, 50]}
-		/>
-	);
+	return <DataGrid components={{ Toolbar }} {...props} />;
 };
 
 export default UsersTable;
