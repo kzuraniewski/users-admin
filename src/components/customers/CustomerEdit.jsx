486c57ca2a00a16ad0efdfb25552df10ead16944
nick-here <zuraniewski.karol@gmail.com>
2022-03-21 13:55:26 +0100

    customer edit input fields

--- src/components/customers/CustomerEdit.jsx
+++ src/components/customers/CustomerEdit.jsx
@@ -6,7 +6,8 @@ import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Paper } from '@mui/material';
+import { Paper, TextField } from '@mui/material';
+import Horizontal from '../utility/Horizontal';
 
 const CustomerEdit = ({ onSave, onReturn }) => {
 	const [tabIndex, setTabIndex] = useState('1');
@@ -17,7 +18,9 @@ const CustomerEdit = ({ onSave, onReturn }) => {
 				Panel klienta
 			</Typography>
 
-			<Paper sx={{ padding: 3, mt: 2 }}>
+			<Paper
+				sx={{ padding: 3, mt: 2, minHeight: 400, display: 'flex', flexDirection: 'column' }}
+			>
 				<TabContext value={tabIndex}>
 					<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
 						<TabList onChange={(e, value) => setTabIndex(value)}>
@@ -27,16 +30,48 @@ const CustomerEdit = ({ onSave, onReturn }) => {
 						</TabList>
 					</Box>
 
-					<TabPanel value='1'>TabPanel 1</TabPanel>
-					<TabPanel value='2'>TabPanel 2</TabPanel>
-					<TabPanel value='3'>TabPanel 3</TabPanel>
+					<form>
+						<TabPanel value='1'>
+							{/* Ogólne */}
+							<TextField label='LP' disabled />
+							<TextField label='Data' />
+							<TextField label='Adres strony' />
+						</TabPanel>
+						<TabPanel value='2'>
+							{/* Dane kontaktowe */}
+							<Horizontal width={450}>
+								<div>
+									<TextField label='Nazwa firmy' />
+									<TextField label='Adres firmy' />
+									<TextField label='NIP' type='number' />
+								</div>
+								<div>
+									<TextField label='Reprezentant' />
+									<TextField label='E-mail' type='email' />
+									<TextField label='Telefon' type='phone' />
+								</div>
+							</Horizontal>
+						</TabPanel>
+						<TabPanel value='3'>
+							<TextField
+								label='Kwota całkowita'
+								type='number'
+								sx={{ display: 'block' }}
+							/>
+
+							<Horizontal width={450}>
+								<TextField label='Rata 1' type='number' />
+								<TextField label='Rata 2' type='number' />
+							</Horizontal>
+						</TabPanel>
+					</form>
 				</TabContext>
 
-				<Box display='flex' justifyContent='flex-end' mt={5}>
-					<Button variant='outlined' onClick={onReturn} sx={{ mr: 2 }}>
+				<Box display='flex' justifyContent='flex-end' mt='auto'>
+					<Button size='large' variant='outlined' onClick={onReturn} sx={{ mr: 2 }}>
 						Wstecz
 					</Button>
-					<Button variant='contained' onClick={() => onSave(0)}>
+					<Button size='large' variant='contained' onClick={() => onSave(0)}>
 						Zapisz
 					</Button>
 				</Box>
