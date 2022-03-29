e271d9d6acc39ad59b87258b7ce53a6583a5f846
nick-here <zuraniewski.karol@gmail.com>
2022-03-29 19:11:23 +0200

    panels lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import theme from './theme.js';
 import { CssBaseline, ThemeProvider, Box } from '@mui/material';
 import Header from './components/Header';
 import Sidenav from './components/Sidenav';
 import { Route, Routes } from 'react-router-dom';
-import Customers from './components/customers/Customers.jsx';
+import Loading from './components/Loading.jsx';
+// import Customers from './components/customers/Customers.jsx';
+
+const Customers = lazy(() => import('./components/customers/Customers.jsx'));
 
 function App() {
 	return (
@@ -12,13 +15,16 @@ function App() {
 			<CssBaseline />
 			<ThemeProvider theme={theme}>
 				<Header />
+
 				<Box display='flex'>
 					<Sidenav />
 
-					<Routes>
-						<Route path='/' element={<div>Strona główna</div>} />
-						<Route path='/klienci/*' element={<Customers />} />
-					</Routes>
+					<Suspense fallback={<Loading />}>
+						<Routes>
+							<Route path='/' element={<div>Strona główna</div>} />
+							<Route path='/klienci/*' element={<Customers />} />
+						</Routes>
+					</Suspense>
 				</Box>
 			</ThemeProvider>
 		</>
